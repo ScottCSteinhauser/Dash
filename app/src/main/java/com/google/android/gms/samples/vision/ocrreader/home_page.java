4b2ee6b99f0ec170e9470b52ecd143f4e34ec4d4
alexlabbane <alex.labbane@tamu.edu>
2020-09-06 06:23:16 -0500

    http request to get all plates

--- app/src/main/java/com/google/android/gms/samples/vision/ocrreader/home_page.java
+++ app/src/main/java/com/google/android/gms/samples/vision/ocrreader/home_page.java
@@ -1,5 +1,6 @@
 package com.google.android.gms.samples.vision.ocrreader;
 
+import android.content.Context;
 import android.content.Intent;
 import android.media.session.MediaSession;
 import android.net.Uri;
@@ -11,9 +12,16 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 
 public class home_page extends AppCompatActivity {
-
+    private static final String TAG = "HomePageActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +29,38 @@ public class home_page extends AppCompatActivity {
         setContentView(R.layout.home_page);
         View background = (View) findViewById(R.id.homeBackground);
         Button button = (Button) findViewById(R.id.button2);
+
+        final Context context = this;
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(home_page.this, OcrCaptureActivity.class));
+                // Instantiate the RequestQueue.
+                RequestQueue queue = Volley.newRequestQueue(context);
+                String url ="https://us-central1-dash-66822.cloudfunctions.net/getPlates";
+
+                // Request a string response from the provided URL.
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                // Display the first 500 characters of the response string.
+                                Log.d(TAG, "!---------------------" + response + "------------------------------!");
+                                Intent intent = new Intent(home_page.this, OcrCaptureActivity.class);
+                                intent.putExtra("PLATE_NUMBERS", response.split(","));
+                                startActivity(intent);
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d(TAG, "!--------------------- Amber Alert Request Failed ------------------------------!");
+                    }
+                });
+
+                // Add the request to the RequestQueue.
+                queue.add(stringRequest);
             }
         });
+
     }
 
 
